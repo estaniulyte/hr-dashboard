01737d7f46e49b5753fd58107a0a331feb3c50e4
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-22 21:52:59 +0300

    feat: add icon components

---
+++ src/components/icons/IconDashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="24" height="24" fill="#DEDEDE" />
+    <g id="Dashboard Desktop" clip-path="url(#clip0_149_462)">
+      <rect width="1440" height="900" transform="translate(-30 -175)" fill="#FAFAFA" />
+      <g id="Sidebar">
+        <g id="Frame 87">
+          <g id="Frame 1">
+            <g id="ic:dashboard">
+              <path
+                id="Vector"
+                d="M3 13H11V3H3V13ZM3 21H11V15H3V21ZM13 21H21V11H13V21ZM13 3V9H21V3H13Z"
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+    <defs>
+      <clipPath id="clip0_149_462">
+        <rect width="1440" height="900" fill="white" transform="translate(-30 -175)" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
