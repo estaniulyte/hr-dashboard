73dfaae018a26ef28e36e1ca7aafed6302349d0a
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-23 20:09:47 +0300

    feat: create svg icon component

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  path: string
+}>()
+</script>
+
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g v-html="path"></g>
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  color: var(--icon-color);
+  height: var(--icon-size);
+  width: var(--icon-size);
+}
+</style>
