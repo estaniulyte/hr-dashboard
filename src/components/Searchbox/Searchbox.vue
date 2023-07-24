69525d393db4f7b49213fbedd3107d978e481cf6
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-24 21:40:56 +0300

    feat: create search box

---
+++ src/components/Searchbox/Searchbox.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon.vue'
+import { searchIconPath } from '@/config/iconPaths'
+</script>
+
+<template>
+  <form>
+    <input type="search" placeholder="Search" />
+    <SvgIcon :path="searchIconPath" class="icon" />
+  </form>
+</template>
+
+<style scoped>
+form {
+  width: 343px;
+  height: 44px;
+  position: relative;
+  display: flex;
+  background-color: var(--background-color);
+  border-radius: 4px;
+  border: var(--border-style);
+  z-index: 0;
+}
+
+input[type='search'] {
+  border: none;
+  width: 100%;
+  align-items: center;
+  flex-shrink: 0;
+  background: transparent;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  padding: 10px 16px;
+  z-index: 0;
+}
+
+input[type='search']::placeholder {
+  color: #b3b3b3;
+}
+
+.icon {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+</style>
