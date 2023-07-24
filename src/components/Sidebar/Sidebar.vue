c7ef4dfb3204b41edfe251cfcf175d3418258259
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-24 11:57:23 +0300

    feat: add Sidebar

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+import { sidebar } from '@/config/sidebar'
+import SvgIcon from '../SvgIcon.vue'
+</script>
+
+<template>
+  <div class="sidebar">
+    <div class="title-container">
+      <h1 class="title">WeHR</h1>
+    </div>
+    <!-- {{ sidebar[0] }} -->
+    <ul class="nav" v-for="item in sidebar">
+      <li>
+        <h3 class="nav-title">{{ item.title }}</h3>
+      </li>
+      <ul class="nav-link" v-for="link in item.items">
+        <li :class="{ active: link.active }">
+          <SvgIcon :path="link.icon"> </SvgIcon>
+          <h4>{{ link.title }}</h4>
+        </li>
+      </ul>
+    </ul>
+  </div>
+</template>
+
+<style scoped\>
+.sidebar {
+  display: flex;
+  width: var(--sidebar-width);
+  max-width: var(--sidebar-width);
+  padding: 20px 0px 272px 0px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100%;
+  background-color: #fafafa;
+  flex-direction: column;
+
+  /* align-items: center; */
+  gap: 39px;
+  flex-shrink: 0;
+}
+
+.title-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.title {
+  color: #000;
+  font-feature-settings: 'salt' on, 'liga' off;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 42px;
+}
+
+.nav {
+  display: flex;
+  flex-direction: column;
+  /* padding: 0px 30px; */
+}
+
+.nav-title {
+  color: var(--secondary-subtitle-color);
+  font-family: Roboto;
+  font-size: 12px;
+  text-transform: uppercase;
+  opacity: 0.5;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+}
+
+.nav-link {
+  display: flex;
+  padding: 16px 30px;
+  align-items: center;
+  text-align: center;
+  color: var(--secondary-subtitle-color);
+}
+
+.nav-link li {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  gap: 24px;
+}
+
+.nav-link:hover{
+  background-color: #f9e2e2;
+  color: var(--main-color-lighten);
+  cursor: pointer;
+}
+
+.nav-link:hover svg{
+  color: var(--main-color-lighten);
+  cursor: pointer;
+}
+
+.active, .active svg {
+  color: var(--main-color);
+}
+
+@media (min-width: 320px) {
+  .sidebar {
+    display: none;
+  }
+}
+
+@media (min-width: 768px) {
+  .sidebar {
+    display: flex;
+    /* //visibility: hidden; */
+  }
+  .nav-link li h4 {
+    display: none;
+  }
+  .title {
+    font-size: 20px;
+  }
+
+  .nav-title {
+    text-align: center;
+    padding-left: 0px;
+    font-size: 10px;
+  }
+
+  .nav-link{
+    justify-content: center;
+  }
+}
+
+@media (min-width: 1024px) {
+ 
+  .nav-link li h4{
+    display: inline;
+  }
+
+  .nav-title {
+    text-align: left;
+    padding-left: 30px;
+  }
+
+  .nav-link{
+    justify-content: left;
+  }
+
+  .title {
+    font-size: 36px;
+  }
+
+}
+</style>
