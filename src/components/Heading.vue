d8589ed4c0c76e9e5e4108974863cf7aa80439b2
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-27 09:15:44 +0300

    feat: create heading

---
+++ src/components/Heading.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <h1 class="title">
+    {{ title }}
+  </h1>
+</template>
+
+<style scoped>
+.title {
+  color: var(--tertiary-color);
+  font-family: var(--main-font);
+  font-style: normal;
+  font-weight: 500;
+  line-height: 42px;
+  margin: 10px 0;
+}
+
+@media (min-width: 390px) {
+  .title {
+    font-size: 20px;
+  }
+}
+
+@media (min-width: 744px) {
+  .title {
+    font-size: 24px;
+  }
+}
+</style>
