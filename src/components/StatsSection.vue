22a7db73bac65ee3e25b901f075fed914583d2a7
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-27 09:08:07 +0300

    feat: create stats section

---
+++ src/components/StatsSection.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { type StatCard } from '@/types/index'
+
+import StatsCard from '@/components/StatsCard.vue'
+
+defineProps<{
+  cards: Array<StatCard>
+}>()
+</script>
+
+<template>
+  <div class="container">
+    <StatsCard
+      v-for="card in cards"
+      :key="card.heading"
+      :title="card.heading"
+      :subtitle="card.subheading"
+      :number="card.number"
+    />
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 390px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+}
+
+@media (min-width: 744px) {
+  .container {
+    display: flex;
+    flex-direction: row;
+    gap: 14px;
+  }
+}
+</style>
