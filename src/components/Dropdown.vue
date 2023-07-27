a797ec890aaa7dc1742222977c5a4ead3297d642
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-27 10:45:27 +0300

    feat: create Dropdown component

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { arrowDownIconPath } from '@/config/iconPaths'
+
+import SvgIcon from '@/components/SvgIcon.vue'
+
+defineProps<{
+  action?: () => void
+}>()
+</script>
+
+<template>
+  <button class="button" @click="action">
+    <slot name="avatar"></slot>
+    <SvgIcon :path="arrowDownIconPath" />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  display: flex;
+  height: 42px;
+  align-items: center;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+  height: 42px;
+}
+
+.button:hover {
+  background-color: var(--background-color);
+  border-radius: var(--border-radius);
+}
+</style>
