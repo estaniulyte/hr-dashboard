1dfe33aca75ca76e967f02855350a613d1726543
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-27 10:45:57 +0300

    feat: create AvatarDropdown

---
+++ src/components/AvatarDropdown.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Avatar from '@/components/Avatar.vue'
+import Dropdown from '@/components/Dropdown.vue'
+
+defineProps<{
+  avatar: {
+    image: string
+    fullName: string
+  }
+}>()
+</script>
+
+<template>
+  <Dropdown class="button">
+    <template #avatar>
+      <Avatar :image="avatar.image" :fullName="avatar.fullName" />
+    </template>
+  </Dropdown>
+</template>
+
+<style scoped>
+.button {
+  display: flex;
+  align-items: center;
+}
+</style>
