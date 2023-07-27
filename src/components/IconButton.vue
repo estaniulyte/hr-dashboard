f44edf02fbc033b0fefda4e49d8903e38e5bf21b
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-27 10:46:31 +0300

    feat: create IconButton

---
+++ src/components/IconButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon.vue'
+
+defineProps<{
+  path: string
+  href?: string
+  tooltip?: string
+}>()
+</script>
+
+<template>
+  <button>
+    <SvgIcon :path="path" class="icon" />
+  </button>
+</template>
+
+<style scoped>
+button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+}
+
+button:hover svg {
+  color: var(--main-color-lighten);
+}
+</style>
