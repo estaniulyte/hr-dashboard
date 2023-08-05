13cbb170f75966b9543dae95e0c2b95053621582
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-05 10:41:43 +0300

    feat: use Axios in app

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 const app = createApp(App)
+
+app.use(VueAxios, axios)
 
 app.use(createPinia())
 
