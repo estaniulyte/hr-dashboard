100f8ec78c2d73b28965b639cfed3685ae1caf1a
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-06 15:00:35 +0300

    change: add Post and Activity types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,54 +1,64 @@
-
 export interface NavItem {
-  title: string;
-  href: string;
-  icon: string;
-  active?: boolean;
-};
+  title: string
+  href: string
+  icon: string
+  active?: boolean
+}
 
 export interface NavItemGroup {
-  title: string;
-  items: NavItem[];
+  title: string
+  items: NavItem[]
 }
 
 export interface IconButton {
-  icon: string;
-  title: string;
-  href: string;
-  tooltip?: string;
+  icon: string
+  title: string
+  href: string
+  tooltip?: string
 }
 
 export interface HeaderConfig {
-  actionButtons: IconButton[];
+  actionButtons: IconButton[]
   userMenu: {
-    image: string;
-    fullName: string;
+    image: string
+    fullName: string
   }
 }
 
 export interface StatCard {
-  heading: string;
-  subheading: string;
-  number: number;
+  heading: string
+  subheading: string
+  number: number
 }
 
 export interface Device {
-  id: number;
-  title: string;
-  description: string;
-  price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
-  images: Array<string>;
+  id: number
+  title: string
+  description: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  brand: string
+  category: string
+  thumbnail: string
+  images: Array<string>
 }
 
 export interface Products {
-  products: Array<Device>;
-  total: number;
-  skip: number;
+  products: Array<Device>
+  total: number
+  skip: number
   // limit: number;
+}
+
+export interface Post {
+  title: string
+  description: string
+  timestamp: string
+}
+
+export interface Activity {
+  recentPost: Post
+  activityCount: number
 }
