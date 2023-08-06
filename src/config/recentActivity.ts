a71029b1b8844c389512980136c4e41f953cc793
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-06 15:00:53 +0300

    feat: create RecentActivity config

---
+++ src/config/recentActivity.ts
@@ -0,0 +1,10 @@
+import { type Activity } from '@/types/types'
+
+export const activity: Activity = {
+  recentPost: {
+    title: "You Posted a New Job",
+    description: "Kindly check the requirements and terms of work and make sure everything is right.",
+    timestamp: "10.40 AM, Fri 10 Sept 2021"
+  },
+  activityCount: 12,
+}
