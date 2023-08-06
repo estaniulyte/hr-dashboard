f50860745e2591f80c385f7a5e14fa4e22b310bf
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-06 15:01:55 +0300

    change: add customizable SvgIcon size

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 defineProps<{
-  path: string
+  path: string,
+  size?: string,
 }>()
 </script>
 
 <template>
   <svg
+    :style="{ '--icon-size': size }"
     width="24"
     height="24"
     viewBox="0 0 24 24"
