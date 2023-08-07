344d0e3221030526528deb0451d2d44944dd6484
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-07 10:32:06 +0300

    feat: create schedule data config

---
+++ src/config/schedule.ts
@@ -0,0 +1,20 @@
+import { type Schedule } from '@/types/types'
+
+export const schedule: Schedule = {
+  priority: [
+    {
+      title: "Review candidate applications",
+      date: "Today - 11.30 AM",
+    }
+  ],
+  other: [
+    {
+      title: "Interview with candidates",
+      date: "Today - 11.30 AM",
+    },
+    {
+      title: "Short meeting with product designer from IT Departement",
+      date: "Today - 09.15 AM",
+    }
+  ]
+}
