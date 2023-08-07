b548874d80922bea31c8322ec93262ef81acaab3
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-07 10:36:56 +0300

    feat: create RecentActivitySection

---
+++ src/components/RecentActivitySection.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ActivityCard from '@/components/cards/ActivityCard.vue'
+
+import { activity } from '@/config/recentActivity'
+</script>
+
+<template>
+  <ActivityCard
+    title="Recent Activity"
+    :post="activity.recentPost"
+    :activityCount="activity.activityCount"
+  />
+</template>
