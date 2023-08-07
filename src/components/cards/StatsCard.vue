9f7e71789b906b7e8c16f67c9414431b0c303932
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-07 10:36:01 +0300

    feat: createStatsCard

---
+++ src/components/cards/StatsCard.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import CardTitle from '@/components/cards/components/CardTitle.vue'
+
+defineProps<{
+  title: string
+  subtitle: string
+  number: number
+}>()
+</script>
+
+<template>
+  <div class="card">
+    <CardTitle :title="title" />
+    <span class="number"> {{ number }}</span>
+    <a class="subtitle">{{ subtitle }}</a>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 100%;
+  max-height: 136px;
+  display: flex;
+  padding: 12px 20px;
+  gap: 16px;
+  flex-direction: column;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.card:hover {
+  transition: var(--transition);
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+}
+
+.number {
+  color: var(--tertiary-colo);
+  font-feature-settings: 'salt' on, 'liga' off;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 28px;
+  font-family: var(--main-font);
+}
+
+.subtitle {
+  color: #ee61cf;
+  font-feature-settings: 'salt' on, 'liga' off;
+  font-family: var(--secondary-font);
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  white-space: nowrap;
+}
+.card:first-of-type {
+  background-color: #ffefe7;
+}
+
+.card:first-of-type .subtitle {
+  color: var(--main-color);
+}
+
+.card:nth-child(2) {
+  background-color: #e8f0fb;
+}
+
+.card:nth-child(2) .subtitle {
+  color: #3786f1;
+}
+
+.card:nth-child(3) {
+  background-color: #fdebf9;
+}
+
+.card:nth-child(3) .subtitle {
+  color: #ee61cf;
+}
+
+@media (min-width: 390px) {
+  .number {
+    font-size: 32px;
+  }
+  .subtitle {
+    font-size: 14px;
+  }
+}
+
+@media (min-width: 1197px) {
+  .number {
+    font-size: 36px;
+  }
+  .subtitle {
+    font-size: 16px;
+  }
+}
+</style>
