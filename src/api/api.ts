c2e31f42dd22a26fb9f5902ad4b36386b0dbac83
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-08-18 12:23:18 +0300

    fix: fix error

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,16 @@
 import axios, { type AxiosResponse } from 'axios';
 
-interface ApiResponse<T> {
-  status: string;
-  data: T;
-}
-
 async function fetchHelper<T>(url: string): Promise<T> {
   try {
-    const response: AxiosResponse<ApiResponse<T>> = await axios.get(url);
+    const response: AxiosResponse<T> = await axios.get(url);
     if (response.data) {
       const data = response.data;
       return data;
     } else {
-      throw new Error('Request failed');
+      throw new Error('Get Request failed');
     }
   } catch (error) {
-    throw new Error('Request failed');
+    throw new Error('Request failed with message' + error);
   }
 }
 
